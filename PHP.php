9281462f3f806586fa7ac7b738c22aab997e28c5
rituraj roj <riturajroj1234@gmail.com>
2022-10-25 16:26:49 +0530

    final commit

--- PHP.php
+++ PHP.php
@@ -6,10 +6,9 @@ $lastname = $_POST["lname"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
-$query = $_POST["query"]
+$query = $_POST["query"];
 $conn = mysqli_connect("localhost", "root", "", "Contact_Form") or die("connection failed");
-$sql = "INSERT INTO databse_table(First_name, Middle_Name, Last_name, Email, Mobile) VALUES ('{$fistname}','{$middlename} ','{$lastname}','{$email}','{$subject}','{$message}','{$query}' )";
+$sql = "INSERT INTO contact_table(First_name, Middle_Name, Last_name, Email,Subject, Message, Query) VALUES ('{$fistname}','{$middlename} ','{$lastname}','{$email}','{$subject}','{$message}','{$query}' )";
 $result = mysqli_query($conn, $sql) or die("Query Failed!");
-header("location: http://localhost/database%20form/HTML.html");
 mysqli_close($conn);
 ?>
